7fb1512634d3f7976280dd821da2cea714de6478
Dollar Bull <ramadhanakhri@gmail.com>
2022-05-24 16:41:27 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "OnB5V9zrRfsofRLl5c091befG"
+consumer_secret = "8knkcWlBOLUwIGFQA7SWR7bdw8cVAANGnTnPIP3ZdKTBT9Nyub"
+access_token = "73816310-ALj8KgVLMhO7T2fI6SqAtbqh4y4GGW65plUmypyBn"
+access_token_secret = "hdFhyigmskMJLtAfsuisb4LoNkWmtOuG0Um3bfRvYrdQ4"
+trigger = "cihuy"
+timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
